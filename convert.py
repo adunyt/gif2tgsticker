edb3f4a00e3a4b49cb7004a525f23ebd21705245
adunyt <adunyt@gmail.com>
2023-06-09 16:28:39 +0300

    refactor: separate main file to several constants to const.py convert function to convert.py

---
+++ convert.py
@@ -0,0 +1,88 @@
+import os
+from pathlib import Path
+import subprocess
+from pprint import pprint
+
+import ffmpeg
+
+def convert_webp_to_apng(filepath: str, apng_delay: str):
+    print("Converting WebP to APNG...")
+
+    p = Path(filepath)
+    output_path = p.with_suffix(".png")
+    subprocess.run(["magick",
+                    *([] if apng_delay == "-1" else ["-delay",  apng_delay]),
+                    filepath,
+                    "apng:" + str(output_path)
+                   ])
+    return str(output_path.absolute())
+
+def process_file(filepath, fps_amount: str, resize_mode: str, smart_limit_duration: str, fallback_pts: str, crf: int, *args):
+    if filepath.lower().endswith(".webp"):
+        filepath = convert_webp_to_apng(filepath, args[0])
+
+    p = Path(filepath)
+
+    job = ffmpeg.input(filepath)
+
+    # 30FPS
+    job = job.filter('fps', fps=fps_amount)
+
+    info = ffmpeg.probe(filepath)
+
+    pprint(info)
+
+    stream = info['streams'][0]
+    fmt = info['format']
+
+    if resize_mode == 'scale':
+        # Try to scale to 512px
+        if stream['width'] >= stream['height']:
+            job = job.filter('scale', 512, -1)
+        else:
+            job = job.filter('scale', -1, 512)
+
+    elif resize_mode == 'pad':
+        if stream['width'] >= stream['height']:
+            job = job.filter('pad', width=512, height='min(ih,512)', x='(ow-iw)/2', y='(oh-ih)/2', color="white@0")
+        else:
+            job = job.filter('pad', width='min(iw,512)', height=512, x='(ow-iw)/2', y='(oh-ih)/2', color="white@0")
+
+
+    if 'duration' in fmt:
+        print("Duration detected: %s" % fmt['duration'])
+        duration = float(fmt['duration'])
+
+        # Try speed up video if it's over 3 seconds
+        if duration > 3.0:
+            job = job.filter('setpts', f"({smart_limit_duration}/{duration})*PTS")
+    else:
+        print("Unable to determine duration")
+        job = job.filter('setpts', f"{fallback_pts}*PTS")
+
+
+    out_path = str(p.with_suffix('.webm'))
+
+    if os.path.exists(out_path):
+        out_path = str(p.with_suffix('.telegram.webm'))
+
+    extra_args = {}
+
+    if crf != -1:
+      extra_args['crf'] = crf
+
+    job = (
+        job
+        .output(
+            out_path,
+            pix_fmt='yuva420p',
+            vcodec='libvpx-vp9',
+            an=None,  # Remove Audio
+            **extra_args,
+        )
+        .overwrite_output()
+    )
+
+    job.run()
+
+    return out_path
--- gif2tgsticker.py
+++ gif2tgsticker.py
@@ -1,20 +1,9 @@
 import os
-from pathlib import Path
-from pprint import pprint
 import tkinter as tk
-import subprocess
-
-import ffmpeg
-
 import tkinterdnd2 as tkd
 
-DEFAULT_FPS = 30
-DEFAULT_SMART_DURATION_LIMIT = '2.9'
-DEFAULT_RESIZE_MODE = 'scale'
-DEFAULT_SPEED_ADJUST_MODE = 'smart'
-DEFAULT_FALLBACK_PTS = '1.0'
-DEFAULT_CRF = -1
-DEFAULT_APNG_DELAY = "-1"
+import convert
+import const
 
 root = tkd.TkinterDnD.Tk()
 root.withdraw()
@@ -22,25 +11,25 @@ root.title('gif2tgsticker')
 root.grid_columnconfigure(0, minsize=600)
 
 fps_var = tk.IntVar()
-fps_var.set(DEFAULT_FPS)
+fps_var.set(const.DEFAULT_FPS)
 
 speed_adjust_mode_var = tk.StringVar()
-speed_adjust_mode_var.set(DEFAULT_SPEED_ADJUST_MODE)
+speed_adjust_mode_var.set(const.DEFAULT_SPEED_ADJUST_MODE)
 
 smart_limit_duration_var = tk.StringVar()
-smart_limit_duration_var.set(DEFAULT_SMART_DURATION_LIMIT)
+smart_limit_duration_var.set(const.DEFAULT_SMART_DURATION_LIMIT)
 
 fallback_pts_var = tk.StringVar()
-fallback_pts_var.set(DEFAULT_FALLBACK_PTS)
+fallback_pts_var.set(const.DEFAULT_FALLBACK_PTS)
 
 resize_mode_var = tk.StringVar()
-resize_mode_var.set(DEFAULT_RESIZE_MODE)
+resize_mode_var.set(const.DEFAULT_RESIZE_MODE)
 
 crf_var = tk.IntVar()
-crf_var.set(DEFAULT_CRF)
+crf_var.set(const.DEFAULT_CRF)
 
 apng_delay_var = tk.StringVar()
-apng_delay_var.set(DEFAULT_APNG_DELAY)
+apng_delay_var.set(const.DEFAULT_APNG_DELAY)
 
 option_box = tk.Frame(root)
 option_box.grid(row=0, column=0)
@@ -122,95 +111,13 @@ def drop(event):
 
             for f in files:
                 if os.path.exists(f):
-                    process_file(f)
+                    out_path = convert.process_file(f, fps_var.get(), resize_mode_var.get(), smart_limit_duration_var.get(), fallback_pts_var.get(), crf_var.get(), apng_delay_var.get())
+                    listbox.insert(0, out_path)
                     print('Dropped file: "%s"' % f)
                 else:
                     print('Not dropping file "%s": file does not exist.' % f)
 
     return event.action
-
-
-def convert_webp_to_apng(filepath: str):
-    print("Converting WebP to APNG...")
-
-    p = Path(filepath)
-    output_path = p.with_suffix(".png")
-    subprocess.run(["magick",
-                    *([] if apng_delay_var.get() == "-1" else ["-delay",  apng_delay_var.get()]),
-                    filepath,
-                    "apng:" + str(output_path)
-                   ])
-    return str(output_path.absolute())
-
-def process_file(filepath):
-    if filepath.lower().endswith(".webp"):
-        filepath = convert_webp_to_apng(filepath)
-
-    p = Path(filepath)
-
-    job = ffmpeg.input(filepath)
-
-    # 30FPS
-    job = job.filter('fps', fps=fps_var.get())
-
-    info = ffmpeg.probe(filepath)
-
-    pprint(info)
-
-    stream = info['streams'][0]
-    fmt = info['format']
-
-    if resize_mode_var.get() == 'scale':
-        # Try to scale to 512px
-        if stream['width'] >= stream['height']:
-            job = job.filter('scale', 512, -1)
-        else:
-            job = job.filter('scale', -1, 512)
-
-    elif resize_mode_var.get() == 'pad':
-        if stream['width'] >= stream['height']:
-            job = job.filter('pad', width=512, height='min(ih,512)', x='(ow-iw)/2', y='(oh-ih)/2', color="white@0")
-        else:
-            job = job.filter('pad', width='min(iw,512)', height=512, x='(ow-iw)/2', y='(oh-ih)/2', color="white@0")
-
-
-    if 'duration' in fmt:
-        print("Duration detected: %s" % fmt['duration'])
-        duration = float(fmt['duration'])
-
-        # Try speed up video if it's over 3 seconds
-        if duration > 3.0:
-            job = job.filter('setpts', f"({smart_limit_duration_var.get()}/{duration})*PTS")
-    else:
-        print("Unable to determine duration")
-        job = job.filter('setpts', f"{fallback_pts_var.get()}*PTS")
-
-
-    out_path = str(p.with_suffix('.webm'))
-
-    if os.path.exists(out_path):
-        out_path = str(p.with_suffix('.telegram.webm'))
-
-    extra_args = {}
-
-    if crf_var.get() != -1:
-      extra_args['crf'] = crf_var.get()
-
-    job = (
-        job
-        .output(
-            out_path,
-            pix_fmt='yuva420p',
-            vcodec='libvpx-vp9',
-            an=None,  # Remove Audio
-            **extra_args,
-        )
-        .overwrite_output()
-    )
-
-    job.run()
-
-    listbox.insert(0, out_path)
 
 listbox.drop_target_register(tkd.DND_FILES, tkd.DND_ALL)
 listbox.dnd_bind('<<Drop>>', drop)
